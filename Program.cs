52741107bcd25386490644a9831cb27570f91645
CharlesSigo <chuck@schooldatebooks.com>
2020-07-06 15:07:02 -0400

    add checks to keep from splitting on math operators

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace OperatorSplitter
@@ -18,15 +19,30 @@ namespace OperatorSplitter
 
         static string SplitStatement(string statement)
         {
-            var matches = Regex.Matches(statement, "\\s?([\\w0-9]+)\\s?(==|>=|<=)?");
             var result = "";
+            var mathOperators = new string[] { "+", "-", "*", "/" };
+            var escapedMathOperators = new string[] { "\\+", "\\-", "\\*", "/" };
+            var equalityOperators = new string[] { "==", ">=", "<=", "!=" };
+
+            var regularExpressionPattern = "\\s?([\\w0-9]+)\\s?(" + String.Join('|', escapedMathOperators) + "|" + String.Join('|', equalityOperators) + ")?";
+            var matches = Regex.Matches(statement, regularExpressionPattern);
 
             foreach(Match match in matches)
             {
                 //result = match.Value;
                 for (var i = 1; i <= match.Groups.Count - 1; i++)
                 {
-                    if (result != "")
+                    var resultEndsWithMathOperator = false;
+
+                    foreach (var mathOperator in mathOperators) {
+                        resultEndsWithMathOperator = result.EndsWith(mathOperator);
+
+                        if (resultEndsWithMathOperator)
+                            break;
+                    }
+
+                    // If the match is not a math operator, result has a value, and the last character of result is not a math operator then add a new line
+                    if (!mathOperators.Contains(match.Groups[i].Value) && result != "" && !resultEndsWithMathOperator)
                         result += "\r\n";
 
                     result += match.Groups[i].Value;
